0a4a2986c8699911c439ceccc401867114ed045d
Jiao Lin <linjiao@ornl.gov>
2016-08-18 12:09:25 -0400

    mcvine/build.sh: fixing python paths and boost paths

--- mcvine/build.sh
+++ mcvine/build.sh
@@ -6,11 +6,18 @@ if ((CORES < 1)); then
     CORES = 1;
 fi
 
+PYVER_MAJOR=${CONDA_PY:0:1}
+PYVER_MINOR=${CONDA_PY:1:1}
+PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
+
 mkdir build
 cd build
 cmake -DCONDA_BUILD=TRUE \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX \
-  -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib .. \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX \
+    -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.so \
+    -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} \
+    -DBOOST_ROOT=$PREFIX \
+    -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib .. \
   && make -j $CORES \
   && make -j$CORES reconfigure-to-include-mcstas-components \
   && make -j$CORES wrap-mcstas-components-cmake \
