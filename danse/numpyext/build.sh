070ddb8e1adf0b24cf15326aeab1c6dea20bc59c
Jiao Lin <linjiao@ornl.gov>
2017-07-02 06:52:32 -0400

    Refs mcvine/mcvine#211. updated recipes for mcvine deps

--- danse/numpyext/build.sh
+++ danse/numpyext/build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-let CORES=`grep -c ^processor /proc/cpuinfo`
-let CORES-=1
-if ((CORES < 1)); then
-    CORES = 1;
-fi
-
 mkdir build
 cd build
-cmake -DCONDA_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX .. && make -j $CORES && make install
+cmake -DCONDA_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX .. && make && make install
