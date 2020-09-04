503611b983cec648960857f6bbc54e86a86cb892
Jiao Lin <jiao.lin@gmail.com>
2020-09-04 12:39:10 -0400

    Refs danse-inelastic/numpyext#12: updated recipe

--- danse/numpyext/build.sh
+++ danse/numpyext/build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+PYVER_MAJOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[0])"`
+PYVER_MINOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[1])"`
+PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
+
 mkdir build
 cd build
-cmake -DCONDA_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX .. && make && make install
+cmake \
+    -DCONDA_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib \
+    .. \
+    && make && make install
+
+
+#   -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.so \
+#   -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} \
