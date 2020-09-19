9d4994514efb2c975f74c4cd7e855bfa78ac49fb
Jiao Lin <jiao.lin@gmail.com>
2020-09-19 10:15:25 -0400

    fix to work with 3.6: journal/build.sh

--- danse/journal/build.sh
+++ danse/journal/build.sh
@@ -15,14 +15,31 @@ PYVER_MAJOR=`python -c "from __future__ import print_function; import sys; print
 PYVER_MINOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[1])"`
 PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
 echo $PYVER
+PY_INCLUDE_DIR=${PREFIX}/include/`ls ${PREFIX}/include/|grep python${PYVER}`
+PY_SHAREDLIB=${PREFIX}/lib/`ls ${PREFIX}/lib/|grep libpython${PYVER}[a-z]*.so$`
+echo $PY_INCLUDE_DIR
+echo $PY_SHAREDLIB
 
-# -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} \
-#    -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.${SHAREDLIB} \
+# 09062020: changed to match that of pyre conda recipe. not tested yet
 mkdir build
 cd build
 cmake \
     -DCONDA_BUILD=TRUE \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_SKIP_INSTALL_RPATH=ON \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib \
+    -DDEPLOYMENT_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib \
+    -DPYTHON_INCLUDE_DIR=${PY_INCLUDE_DIR} \
+    -DPYTHON_LIBRARY=${PY_SHAREDLIB} \
     .. \
     && make && make install
+
+# -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PYVER} \
+#    -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PYVER}.${SHAREDLIB} \
+#cmake \
+#    -DCMAKE_SKIP_INSTALL_RPATH=ON \
+#    -DCONDA_BUILD=TRUE \
+#    -DCMAKE_INSTALL_PREFIX=$PREFIX -DDEPLOYMENT_PREFIX=$PREFIX -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib \
+#    .. \
+#    && make && make install
