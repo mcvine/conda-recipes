7024e081325880c53c302e10673271a3feaf34ea
Jiao Lin <jiao.lin@gmail.com>
2020-09-25 22:15:16 -0400

    Refs mcvine/mcvine#392. updated recipe

--- mcvine-core/build.sh
+++ mcvine-core/build.sh
@@ -10,6 +10,7 @@ PYVER_MAJOR=`python -c "from __future__ import print_function; import sys; print
 PYVER_MINOR=`python -c "from __future__ import print_function; import sys; print(sys.version_info[1])"`
 PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
 echo $PYVER
+echo $PREFIX
 PY_INCLUDE_DIR=${PREFIX}/include/`ls ${PREFIX}/include/|grep python${PYVER}`
 PY_SHAREDLIB=${PREFIX}/lib/`ls ${PREFIX}/lib/|grep libpython${PYVER}[a-z]*.so$`
 echo $PY_INCLUDE_DIR
@@ -24,7 +25,7 @@ cmake -DCONDA_BUILD=TRUE \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib \
     .. \
-  && make -j $CORES \
+  && make -j$CORES VERBOSE=1 \
   && make -j$CORES reconfigure-to-include-mcstas-components \
   && make -j$CORES wrap-mcstas-components-cmake \
   && make -j$CORES \
