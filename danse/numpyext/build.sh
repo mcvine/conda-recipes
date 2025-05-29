4bd25a90218b5633c44eae7af58457b0fa507e93
Patrou, Maria <patroum@ornl.gov>
2025-05-29 10:08:43 -0400

    numpy ext build path update

--- danse/numpyext/build.sh
+++ danse/numpyext/build.sh
@@ -6,7 +6,6 @@ PYVER=${PYVER_MAJOR}.${PYVER_MINOR}
 echo $PYVER
 PY_INCLUDE_DIR=${PREFIX}/include/`ls ${PREFIX}/include/|grep python${PYVER}`
 PY_SHAREDLIB=${PREFIX}/lib/`ls ${PREFIX}/lib/|grep libpython${PYVER}[a-z]*.so$`
-PY_INCLUDE_DIR="/home/dwp/miniconda3/envs/conda-recipe/include/python3.10/;/home/dwp/miniconda3/envs/conda-recipe/lib/python3.10/site-packages/numpy/_core/include"
 echo $PY_INCLUDE_DIR
 echo $PY_SHAREDLIB
 
